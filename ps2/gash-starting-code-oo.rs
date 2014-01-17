ff0d81ee0f2f39b675ab41aa0a43bb9902b25df4
Weilin <mzweilin@gmail.com>
2014-01-17 11:02:55 -0500

    Removed history feature in ps2 starting code. Simplified the argument processing code.

--- ps2/gash-starting-code-oo.rs
+++ ps2/gash-starting-code-oo.rs
@@ -11,20 +11,18 @@
 
 extern mod extra;
 
-use std::{io, run, os, libc};
+use std::{io, run, os};
 use std::io::buffered::BufferedReader;
 use std::io::stdin;
 use extra::getopts;
 
 struct Shell {
-    history: ~[~str],
     cmd_prompt: ~str,
 }
 
 impl Shell {
     fn new(prompt_str: &str) -> Shell {
         Shell {
-            history: ~[],
             cmd_prompt: prompt_str.to_owned(),
         }
     }
@@ -39,10 +37,6 @@ impl Shell {
             let line = stdin.read_line().unwrap();
             let cmd_line = line.trim().to_owned();
             
-            if cmd_line.len() > 0 {
-                self.history.push(cmd_line.to_owned());
-            }
-            
             let mut argv: ~[~str] =
                 cmd_line.split(' ').filter_map(|x| if x != "" { Some(x.to_owned()) } else { None }).to_owned_vec();
         
@@ -50,13 +44,6 @@ impl Shell {
                 let program = argv.remove(0);
                 match program {
                     ~"exit"     =>  { return; }
-                    
-                    ~"history"  =>  {
-                                        for i in range(0, self.history.len()) {
-                                            println(format!("{:u} {:s}", i+1, self.history[i]));
-                                        }
-                                    }
-                    
                     _           =>  { self.run_cmd(program, argv); }
                     
                 }
@@ -89,29 +76,17 @@ impl Shell {
 }
 
 fn get_cmdline_from_args() -> Option<~str> {
-    fn print_usage(program: &str) {
-        println!("Usage: {:s} [options]", program);
-        println("-c            \tcommand");
-        println("-h --help     \tUsage");
-    }
-    
     /* Begin processing program arguments and initiate the parameters. */
     let args = os::args();
-    let program = args[0].clone();
     
     let opts = ~[
-        getopts::optopt("c"),
-        getopts::optflag("h"),
-        getopts::optflag("help")
+        getopts::optopt("c")
     ];
+    
     let matches = match getopts::getopts(args.tail(), opts) {
         Ok(m) => { m }
         Err(f) => { fail!(f.to_err_msg()) }
     };
-    if matches.opt_present("h") || matches.opt_present("help") {
-        print_usage(program);
-        unsafe { libc::exit(1); }
-    }
     
     if matches.opt_present("c") {
         let cmd_str = match matches.opt_str("c") {

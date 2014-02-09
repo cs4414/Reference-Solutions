60699199915729a4677e88ec2de6c83d141d2759
Weilin <mzweilin@gmail.com>
2014-02-09 17:58:54 -0500

    draft version of ps3 starting code.

--- ps3/zhtta-from-zhttpto.rs
+++ ps3/zhtta-from-zhttpto.rs
@@ -1,38 +1,94 @@
 //
-// zhttpto.rs
+// zhtta.rs
 //
-// Reference solution for PS1
+// Starting code for PS3
 // Running on Rust 0.9
 //
 // Note that this code has serious security risks!  You should not run it 
 // on any system with access to sensitive files.
-//
-// Special thanks to Kiet Tran for providing code we incorporated into this.
 // 
 // University of Virginia - cs4414 Spring 2014
 // Weilin Xu and David Evans
-// Version 0.3
+// Version 0.4
 #[feature(globs)];
+extern mod extra;
+
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
 use std::{os, str};
+//use std::hashmap::HashMap;
+
+use extra::arc::MutexArc;
+//use extra::priority_queue::PriorityQueue;
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
 static mut visitor_count: uint = 0;
 
+struct HTTP_Request {
+    //priority: uint,
+    stream: Option<std::io::net::tcp::TcpStream>,
+    //peer_name: std::io::net::ip::SocketAddr,
+    path: ~std::path::PosixPath
+}
+
+
+/*
+impl Ord for HTTP_Request {
+    fn lt(&self, other: &HTTP_Request) -> bool {
+        if self.priority > other.priority { true } else { false }
+    }
+}
+*/
+
 fn main() {
+    let req_queue: ~[HTTP_Request] = ~[];
+    let shared_req_queue = MutexArc::new(req_queue);
+    
+    let (notify_port, shared_notify_chan) = SharedChan::new();
+    
+    let req_queue_get = shared_req_queue.clone();
+    do spawn {
+        loop {
+            notify_port.recv();
+            unsafe {
+                req_queue_get.unsafe_access( |req_queue| {
+                
+                    match req_queue.shift_opt() {
+                        None => { /* do nothing */ }
+                        Some(mut req) => {
+                                        println(format!("serve file: {:?}", req.path));
+                                        req.stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".as_bytes());
+
+                                        let contents = File::open(req.path).read_to_end();
+                                        req.stream.write(contents);
+                                     }
+                    }
+                 });
+            }
+        }
+    }
+
     let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).expect("Address error.");
     let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
     
     println(format!("Listening on [{:s}] ...", addr.to_str()));
     
     for stream in acceptor.incoming() {
+        let (queue_port, queue_chan) = Chan::new();
+        queue_chan.send(shared_req_queue.clone());
+        
+        let notify_chan = shared_notify_chan.clone();
+        
         // Spawn a task to handle the connection
         do spawn {
             unsafe {
                 visitor_count += 1;
             }
+            
+            let shared_req_queue = queue_port.recv();
+            //let local_req_queue = shared_req_queue.get();
+          
             let mut stream = stream;
             
             match stream {
@@ -55,7 +111,7 @@ fn main() {
                 let path_str = "." + req_group[1].to_owned();
                 println(format!("Request for path: \n{:?}", path_str));
                 
-                let mut path_obj = os::getcwd();
+                let mut path_obj = ~os::getcwd();
                 path_obj.push(path_str.clone());
                 
                 let ext_name = match path_obj.extension_str() {
@@ -88,11 +144,19 @@ fn main() {
                                     </body></html>\r\n", IP, PORT);
                     stream.write(response.as_bytes());
                 } else {
-                    println(format!("serve file: {:s}", path_str));
-                    stream.write("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n".as_bytes());
-
-                    let contents = File::open(&path_obj).read_to_end();
-                    stream.write(contents);
+                    // request scheduling
+                    
+                    //let local_req_queue = shared_req_queue.get();
+                    let req = HTTP_Request{stream: stream, path: path_obj.clone()};
+                    let (req_port, req_chan) = Chan::new();
+                    req_chan.send(req);
+                    unsafe {
+                        shared_req_queue.unsafe_access(|local_req_queue| {
+                            let req: HTTP_Request = req_port.recv();
+                            local_req_queue.push(req);
+                        });
+                    }
+                    notify_chan.send(());
                 }
             }
             println!("connection terminates")
